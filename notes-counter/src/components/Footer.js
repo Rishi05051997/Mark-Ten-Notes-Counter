1741afbaa20432862f1057ac7d7ecdcbc4f3a97a
rishi05051997 <vrushabhdhatral10@gmail.com>
2021-07-22 11:40:27 +0530

    footer added

---
+++ notes-counter/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <h3>App Made  with 💗 <a  href="https://www.linkedin.com/in/vrushabh-dhatrak-328ab0148/">Vrushabh Dhatrak</a></h3>
+            <p>Copyright @ <a  href="https://www.linkedin.com/in/vrushabh-dhatrak-328ab0148/">Vrushabh Dhatrak</a></p>
+        </div>
+    )
+}
